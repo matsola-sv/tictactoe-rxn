4b8139ad4f4efaa6f199c1d138cfbf1bf9331595
peacewarrior <you.mail@gmail.com>
2024-08-02 18:05:18 +0200

    time viewer component

---
+++ src/components/common/clock/TimeViewer.tsx
@@ -0,0 +1,19 @@
+import {FC} from "react";
+import "../../../assets/css/Clock.css";
+import TimeUnit from "./TimeUnit";
+interface StopwatchProps {
+    hours: number,
+    minutes: number,
+    seconds: number
+}
+
+const TimeViewer: FC<StopwatchProps> = ({hours, minutes, seconds}) => {
+    return (
+        <div className="time-viewer">
+            <TimeUnit value={hours} label={"hours"}/>
+            <TimeUnit value={minutes} label={"minutes"}/>
+            <TimeUnit value={seconds} label={"seconds"} separator={""}/>
+        </div>
+    )
+}
+export default TimeViewer;
