e9c6733a942807a9f6b1f5bf63e3cccd423b657e
matsola-sv <sergey.macola@gmail.com>
2024-12-03 14:11:27 +0100

    Feat: add normalizeError and logError methods

--- src/components/Common/Error/Message/Message.tsx
+++ src/components/Common/Error/Message/Message.tsx
@@ -2,13 +2,13 @@ import {FC} from "react";
 import "./Message.css";
 
 interface ErrorMessageProps {
-    message: string;
+    error: Error;
 }
 
-const ErrorMessage: FC<ErrorMessageProps> = ({ message }) => {
+const ErrorMessage: FC<ErrorMessageProps> = ({ error }) => {
     return (
         <div className="error-message">
-            <strong>Error:</strong> {message}
+            <strong>Error:</strong> {error.message}
         </div>
     );
 };
---
+++ src/utils/error.ts
@@ -0,0 +1,14 @@
+export const normalizeError = (error: unknown, defaultMessage: string = "An unknown error"): Error => {
+    if (error instanceof Error) {
+        return error;
+    }
+
+    if (typeof error === 'string') {
+        return new Error(error);
+    }
+    return new Error(defaultMessage);
+};
+
+export const logError = (error: unknown, defaultMessage?: string): void => {
+    console.error(normalizeError(error, defaultMessage));
+}
